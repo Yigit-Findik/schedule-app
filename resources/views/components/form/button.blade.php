de3ede723fcc032742a3e8f5130d3f0c17d696fe
Yigit Findik <yigit8@live.nl>
2023-03-08 12:17:05 +0100

    - created form components - register is working - added username to users migration

---
+++ resources/views/components/form/button.blade.php
@@ -0,0 +1,8 @@
+<x-form.field>
+    <button
+        class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 rounded-full text-xs font-semibold text-white uppercase py-2 px-10" type="submit"
+    >
+        {{ $slot }}
+    </button>
+
+</x-form.field>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 // Home
 Route::get('/', [ScheduleController::class, 'index'])->name('home');
+
+// Login
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+// Logout
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+// Registration
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
