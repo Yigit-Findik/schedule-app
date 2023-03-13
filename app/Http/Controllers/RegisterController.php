4e329ac61edd2c9c2393f321fa4088802fd491e3
Yigit Findik <yigit8@live.nl>
2023-03-13 14:23:33 +0100

    - fix profile picture upload

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -18,16 +19,19 @@ public function create()
     public function store()
     {
         $attributes = request()->validate([
-            'name' => 'required',
-            'username' => 'required',
-            'email' => 'required|email',
-            'role_id' => 'required',
-            'password' => 'required'
+            'name' => 'required|max:255',
+            'username' => 'required|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
+            'profile_thumbnail' => 'required|image|max:1024',
+            'password' => 'required|min:7|max:255',
+            'role_id' => ['required', Rule::in(Role::all()->pluck('id')->toArray())]
         ]);
 
-        $attributes['profile_thumbnail'] = request()->file('profile_thumbnail')->store('profile_thumbnail');
+        $attributes['profile_thumbnail'] = request()->file('profile_thumbnail')->store('profile_thumbnails');
+        $user = User::create($attributes);
 
-        User::create($attributes);
+        // After creating the user, log the user in
+        auth()->login($user);
 
         return redirect('/')->with('success', 'Your account has been created!');
     }
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -3,7 +3,7 @@
         <main class="max-w-lg mx-auto mt-10">
             <x-panel>
                 <h1 class="text-center font-bold text-xl">Register</h1>
-                <form method="POST" action="/register" class="mt-10">
+                <form method="POST" action="/register" class="mt-10" enctype="multipart/form-data">
                     @csrf
                     <x-form.input name="name" type="text" autocomplete="name"/>
                     <x-form.input name="username" type="text" autocomplete="username"/>
